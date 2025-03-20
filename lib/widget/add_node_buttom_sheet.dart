1fad1160c2101c6e63bfd45d25ae9ed2ac012bc1
mohammed99-m <markoriobrazil@gmail.com>
2025-03-21 00:54:07 +0300

    finish notes button field

--- lib/widget/add_node_buttom_sheet.dart
+++ lib/widget/add_node_buttom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hivep2/constatns.dart';
+import 'package:hivep2/widget/custom_button.dart';
 import 'package:hivep2/widget/custom_text_field.dart';
 
 class AddNodeButtomSheet extends StatelessWidget {
@@ -8,14 +10,29 @@ class AddNodeButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          // SizedBox(height:32,),
-          // Text("Helll"),
-          CustomTextField(hintText:'title',maxLines: 1,),
-          SizedBox(height:10,),
-          CustomTextField(hintText:'content',maxLines: 5,),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // SizedBox(height:32,),
+            // Text("Helll"),
+            CustomTextField(
+              hintText: 'title',
+              maxLines: 1,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              hintText: 'content',
+              maxLines: 5,
+            ),
+        
+            SizedBox(height:50,),
+        
+            CustomButton(),
+            SizedBox(height:10,),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widget/custom_button.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:hivep2/constatns.dart';
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: PrimaryColor
+      ),
+      child: const Center(child: Text("Add",style:TextStyle(fontSize:20,fontWeight:FontWeight.bold,color: Colors.black),),),
+    );
+
+  }
+}
